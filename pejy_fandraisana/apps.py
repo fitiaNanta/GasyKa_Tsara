81aa2bea1a888992357334a74d2eeb8b562b001b
Randriamanjaka123 <fitia.randriamanjaka@esti.mg>
2025-07-07 15:31:46 +0300

    first commit

---
+++ pejy_fandraisana/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PejyFandraisanaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pejy_fandraisana'
---
+++ pejy_fandraisana/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pejyfandraisana(request):
+    return render(request, "pejy_fandraisana/fandraisana.html")
+
